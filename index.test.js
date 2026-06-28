4efbba20c4ff4f3ed049a87f5517a49df2d96fc7
rodolphe <had-sublet-frozen@duck.com>
2026-06-28 20:30:08 +0200

    fix(test): parse output.json and _output.json to compare json object

--- index.test.js
+++ index.test.js
@@ -2,8 +2,8 @@ import fs from "fs";
 
 describe("compare _output.json and output.json", () => {
   it("should be the same", () => {
-    const _output = fs.readFileSync("./_output.json", "utf8");
-    const output = fs.readFileSync("./output.json", "utf8");
+    const _output = JSON.parse(fs.readFileSync("./_output.json", "utf8"));
+    const output = JSON.parse(fs.readFileSync("./output.json", "utf8"));
 
     expect(_output).toEqual(output);
   });
